c426acffaf6326cd5087d8e18ad1d241d8b7aa0f
Fabio Beneditto <fabiobeneditto@gmail.com>
2015-05-18 20:45:34 -0300

    Initial Commit
    Open Repository

---
+++ Cadastro/Controllers/ClienteController.cs
@@ -0,0 +1,21 @@
+﻿using Cadastro.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cadastro.Controllers
+{
+    public class ClienteController : Controller
+    {
+        CadastroEntities db = new CadastroEntities();
+
+        // GET: Cliente
+        public ActionResult Index()
+        {
+            List<Cliente> lstClientes = db.Cliente.ToList<Cliente>();
+            return View(lstClientes);
+        }
+    }
+}
