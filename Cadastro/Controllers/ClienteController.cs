57e7ce90b0a11ad6b98e59678908221ac8a29561
Fabio Beneditto <fabiobeneditto@gmail.com>
2015-05-22 20:34:33 -0300

    Create Add MVC

--- Cadastro/Controllers/ClienteController.cs
+++ Cadastro/Controllers/ClienteController.cs
@@ -17,5 +17,30 @@ namespace Cadastro.Controllers
             List<Cliente> lstClientes = db.Cliente.ToList<Cliente>();
             return View(lstClientes);
         }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="Nome, DataNascimento, Email, Telefone")] Cliente cliParaSalvar)
+        {
+            try
+            {
+                if(ModelState.IsValid)
+                {
+                    db.Cliente.Add(cliParaSalvar);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Não foi possível salvar o CLiente." + ex.Message);
+            }
+            return View();
+        }
     }
 }
