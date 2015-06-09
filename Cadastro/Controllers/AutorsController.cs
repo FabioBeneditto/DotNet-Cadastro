f17ef0b2e26a3ba13f68e02d5632dc0e76462629
Fabio Beneditto <fabiobeneditto@gmail.com>
2015-06-08 23:00:28 -0300

    Add Autors MVC

---
+++ Cadastro/Controllers/AutorsController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Cadastro.Models;
+
+namespace Cadastro.Controllers
+{
+    public class AutorsController : Controller
+    {
+        private CadastroEntities db = new CadastroEntities();
+
+        // GET: Autors
+        public ActionResult Index()
+        {
+            return View(db.Autor.ToList());
+        }
+
+        // GET: Autors/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Autor autor = db.Autor.Find(id);
+            if (autor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(autor);
+        }
+
+        // GET: Autors/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Autors/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CodAutor,Nome,CPF,DtNascimento")] Autor autor)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Autor.Add(autor);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Não foi possível salvar o Autor." + ex.Message);
+            }
+
+            return View(autor);
+        }
+
+        // GET: Autors/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Autor autor = db.Autor.Find(id);
+            if (autor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(autor);
+        }
+
+        // POST: Autors/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CodAutor,Nome,CPF,DtNascimento")] Autor autor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(autor).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(autor);
+        }
+
+        // GET: Autors/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Autor autor = db.Autor.Find(id);
+            if (autor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(autor);
+        }
+
+        // POST: Autors/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Autor autor = db.Autor.Find(id);
+            db.Autor.Remove(autor);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
